706832f5879a1316c2c320b04708eb71576cf230
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-10-05 14:19:13 -0500

    adds pseudocode to js

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// create variables
+    // var city
+
+// create var for APIKey
+
+// function save to local storage
+    // save city name into blank array
+
+// function to get city name out of local storage
+    // call fetch functions?
+
+// function to fetch data for current weather
+    // http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+        // get city name / date
+        // get icon
+        // get temp, wind, and humidity
+        // create elements (h2, icon, 3 p)
+        // modify elements
+        // append elements
+        // call save to local storage function
+        // for loop for array of cities searched
+            // create button
+            // modify button
+            // append button
+    // input.value = ""
+
+// function to fetch data for 5 day forecast
+    // http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+        // get date
+        // get icon
+        // get temp, wind, and humidity
+        // create elements (h2, icon, 3 p)
+        // modify elements
+        // append elements
+
+// function for submit form
+    // prevent default
+    // modify var city to input.value
+    // call fetch functions with var city
+
+// submit listener for form
+// click listener for search history cities
+    // use text content in <button> to create var that will be used to fetch the data above
